cf1bb1dfc3242c8ce0dfa7a8be0fcca63463424c
nestormeca <nestor.m3@gmail.com>
2021-12-19 20:01:11 -0300

    repa

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -10,7 +10,7 @@ const LeftArrow = () => {
     <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleLeft}
-        onClick={()=>scrollPrev()}
+        onClick={() => scrollPrev()}
         fontSize="2xl"
         cursor="pointer"
       />
@@ -24,7 +24,7 @@ const RightArrow = () => {
     <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleRight}
-        onClick={()=> scrollNext()}
+        onClick={() => scrollNext()}
         fontSize="2xl"
         cursor="pointer"
       />
@@ -32,23 +32,25 @@ const RightArrow = () => {
   );
 };
 
-
-
-const ImageScrollbar = ({ data }) => (
-
-  <ScrollMenu
-    LeftArrow={LeftArrow}
-    RightArrow={RightArrow}
-    style={{ overflow: "hidden" }}
-  >
+export default function ImageSrollbar({ data }) {
+  return (
+    <ScrollMenu
+      LeftArrow={LeftArrow}
+      RightArrow={RightArrow}
+      style={{ overflow: "hidden" }}
+    >
       {data.map((item) => (
-
-      <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
-          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
-      </Box>
+        <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+          <Image
+            placeholder="blur"
+            blurDataURL={item.url}
+            src={item.url}
+            width={1000}
+            height={500}
+            sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+          />
+        </Box>
       ))}
-
-  </ScrollMenu>
-);
-
-export default ImageScrollbar;
+    </ScrollMenu>
+  );
+}
